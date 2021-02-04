1de72de739e652da02503db51c3487a51c29ac0e
ayumu.maruyama <nanaya_shiqui@yahoo.co.jp>
2021-02-04 07:00:46 +0000

    validation

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,7 +1,7 @@
-@if (conut($errors) >0)
+@if (count($errors) >0)
     <ul class="alert alert-danger" role="alert">
         @foreach ($errors->all() as $error)
-            <li class="ml-4">{{ $erromr }}</li>
+            <li class="ml-4">{{ $error }}</li>
         @endforeach
     </ul>
 @endif
